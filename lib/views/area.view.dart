b48c1ddbec48f74328641fdff158a50827689465
Felipe <fs.cicatriz@hotmail.com>
2020-10-21 02:01:37 -0300

    Adição de links da área Cível

--- lib/views/area.view.dart
+++ lib/views/area.view.dart
@@ -1,6 +1,8 @@
 import 'package:MPSP/bloc/navigation.bloc/navigation.bloc.dart';
 import 'package:MPSP/config/pallet.dart';
 import 'package:flutter/material.dart';
+import 'package:MPSP/widgets/iconContainer.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Area extends StatelessWidget with NavigationStates {
   final Function onMenuTap;
@@ -53,7 +55,7 @@ class Area extends StatelessWidget with NavigationStates {
                   child: Container(
                     //alignment: Alignment.topCenter,
                     margin: const EdgeInsets.symmetric(horizontal: 8),
-                    color: Palett.vermelhompsp,
+                    //color: Palett.vermelhompsp,
                     width: 100,
                     child: Column(
                       children: [
@@ -76,35 +78,10 @@ class Area extends StatelessWidget with NavigationStates {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black.withOpacity(0.5),
-                                      spreadRadius: 0.5,
-                                      blurRadius: 10,
-                                      offset: Offset(0, 5),
-                                    )
-                                  ],
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image: AssetImage('assets/img/atos.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image:
-                                        AssetImage('assets/img/destaques.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
+                            IconContainer('assets/img/atos.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/Eoyzwst8bdVCnQElp_DrXPUBV6ZX1mgVkdnjhpfq3Xa4xw?e=QS6MaB'),
+                            IconContainer('assets/img/destaques.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/Eh5GSqxq4oJIgqVCbBUThC0BSegNiOFzR2_AKqukvvbt4g?e=7GZ1ja')
                           ],
                         ),
                         SizedBox(height: 5),
@@ -129,27 +106,10 @@ class Area extends StatelessWidget with NavigationStates {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image:
-                                        AssetImage('assets/img/falencias.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image: AssetImage('assets/img/familia.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
+                            IconContainer('assets/img/falencias.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/Eony4SkBR_BBpd5dpGj1Y_gBL_B4hXpJy4zcn7-O6JstWg?e=GSc4cy'),
+                            IconContainer('assets/img/familia.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/Eu-S_WjOk7hIoFfE9dwQ4ysBUqnewaqRuHQNLvSK8Xy9AA?e=vOgxwn'),
                           ],
                         ),
                         SizedBox(height: 5),
@@ -174,28 +134,10 @@ class Area extends StatelessWidget with NavigationStates {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image:
-                                        AssetImage('assets/img/fundacoes.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image:
-                                        AssetImage('assets/img/mandados.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
+                            IconContainer('assets/img/fundacoes.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/EsWz3udkDPRJu12TcO2PeG0BE6-_RK32VO1-GM8BQIle7Q?e=2sIPWc'),
+                            IconContainer('assets/img/mandados.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/EgFnAUiu4mlJjhjReWy6X78Bd6tv7UqqgSyYhyGHYLSENA?e=qqhcv3'),
                           ],
                         ),
                         SizedBox(height: 5),
@@ -220,28 +162,10 @@ class Area extends StatelessWidget with NavigationStates {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image: AssetImage(
-                                        'assets/img/processocivil.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image: AssetImage(
-                                        'assets/img/registrospublicos.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
+                            IconContainer('assets/img/processocivil.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/EtZfXKNngM1Hu2n4WUSBE88Ba24SgVb6Rz3K_nKtwS1yTw?e=8FDaGW'),
+                            IconContainer('assets/img/registrospublicos.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/EoaU-tbGuaREpWZ82XxIB1IB0dws-vO0ziOBmi7xby8ooQ?e=H20yOE'),
                           ],
                         ),
                         SizedBox(height: 5),
@@ -266,27 +190,10 @@ class Area extends StatelessWidget with NavigationStates {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image:
-                                        AssetImage('assets/img/roteiros.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image: AssetImage('assets/img/acordos.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
+                            IconContainer('assets/img/roteiros.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/EveT_cf0gDJEpmmDZHlxq0oBib9BvWpfM4VrdVKM9Z_gEw?e=l0X2SF'),
+                            IconContainer('assets/img/acordos.png',
+                                'https://mpspbr.sharepoint.com/:f:/s/g_caocivel/EpZmUCV_zt9ItZrjSLjBVZcB3jtbNmcHb5qAPNvhqn2MMw?e=fz2EEW'),
                           ],
                         ),
                         SizedBox(height: 5),
@@ -311,25 +218,29 @@ class Area extends StatelessWidget with NavigationStates {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image: AssetImage('assets/img/dna.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Palett.azulmpsp,
-                                  image: DecorationImage(
-                                    image: AssetImage('assets/img/atos.png'),
-                                  )),
-                              width: boxSize,
-                              height: boxSize,
+                            IconContainer('assets/img/dna.png',
+                                'https://nam02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fmpspbr.sharepoint.com%2Fsites%2Fg_examedna&data=02%7C01%7CAnaAdorno%40mpsp.mp.br%7Cafa23a4a709d44fac35808d73e006bc7%7C2dbd8499508d4b76a31dca39cb3d8f1d%7C0%7C0%7C637046043850379624&sdata=WXB2LpYWhQdXI2D4NIIBcP869%2BvNMB79OdJoLuLUcJA%3D&reserved=0'),
+                            GestureDetector(
+                              onTap: () => launch(
+                                  "https://mpspbr.sharepoint.com/:f:/s/g_caocivel/Eoyzwst8bdVCnQElp_DrXPUBV6ZX1mgVkdnjhpfq3Xa4xw?e=QS6MaB"),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.5),
+                                        spreadRadius: 0.5,
+                                        blurRadius: 10,
+                                        offset: Offset(0, 5),
+                                      )
+                                    ],
+                                    color: Palett.azulmpsp,
+                                    image: DecorationImage(
+                                      image: AssetImage('assets/img/dna.png'),
+                                    )),
+                                width: boxSize,
+                                height: boxSize,
+                              ),
                             ),
                           ],
                         ),
--- lib/widgets/iconContainer.dart
+++ lib/widgets/iconContainer.dart
@@ -1,8 +1,43 @@
-// class IconContainer extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
+import 'package:flutter/material.dart';
+import 'package:MPSP/config/pallet.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-//     );
-//   }
-// }
+class IconContainer extends StatelessWidget {
+  static const double boxSize = 80;
+
+  IconContainer(String img, String link);
+
+  @override
+  Widget build(BuildContext context) {
+    var img;
+    var link;
+    return GestureDetector(
+      onTap: () => launch("$link"),
+      child: Container(
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 0.5,
+                blurRadius: 10,
+                offset: Offset(0, 5),
+              )
+            ],
+            color: Palett.azulmpsp,
+            image: DecorationImage(
+              image: AssetImage('$img'),
+            )),
+        width: boxSize,
+        height: boxSize,
+      ),
+    );
+  }
+}
+
+class IconTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
